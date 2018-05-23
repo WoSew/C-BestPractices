5d972b5adfcdd244d84c4288c3354252f75733eb
WoSew <wojseweryn@gmail.com>
2018-05-23 22:01:27 +0200

    Properties

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -14,11 +14,17 @@ namespace Acme.Biz
     /// </summary>
     public class Product
     {
+        
+        public const double InchesPerMEter = 39.37;
+        public readonly decimal MinimumPrice;
+
         #region Constructors
         public Product()
         {
             Console.WriteLine("Product instance created");
             //this.ProductVendor = new Vendor();
+            this.MinimumPrice = .96m;
+            this.Category = "Tools";
         }
 
         public Product(int productId, string productName, string description) : this()
@@ -28,16 +34,47 @@ namespace Acme.Biz
             this.Description = description;
 
             Console.WriteLine("Product instance has a name: " + ProductName);
+            if (ProductName.StartsWith("Game"))
+            {
+                this.MinimumPrice = 9.99m;
+            }
         }
         #endregion
 
         #region Properties
+        private DateTime? availabilityDate;
+
+        public DateTime? AvailabilityDate
+        {
+            get { return availabilityDate; }
+            set { availabilityDate = value; }
+        }
+
+
         private string productName;
 
         public string ProductName
         {
-            get { return productName; }
-            set { productName = value; }
+            get
+            {
+                var formattedValue = productName?.Trim();
+                return formattedValue;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    ValidationMassage = "Product Name must be at least 3 characters.";
+                }
+                else if (value.Length > 20)
+                {
+                    ValidationMassage = "Product Name cannot be more than 20 characters.";
+                }
+                else
+                {
+                    productName = value;
+                }
+            }
         }
 
         private string description;
@@ -61,7 +98,7 @@ namespace Acme.Biz
         public Vendor ProductVendor
         {
             get
-            {
+            {//lazy loading
                 if (productVendor == null)
                 {
                     productVendor = new Vendor();
@@ -70,7 +107,14 @@ namespace Acme.Biz
             }
             set { productVendor = value; }
         }
-            
+
+        internal string Category { get;  set; }
+        public int SequenceNumber { get; set; } = 1;
+
+        public string ProductCode => this.Category + "-" + this.SequenceNumber;
+
+        public string ValidationMassage { get; private set; }
+
         #endregion
 
         public string SayHello()
@@ -83,7 +127,8 @@ namespace Acme.Biz
 
             var result = LoggingService.LogAction("saying hello");
 
-            return "Hello " + ProductName + " (" + ProductId + "): " + Description;
+            return "Hello " + ProductName + " (" + ProductId + "): " + Description + " Available on: " +
+                   AvailabilityDate?.ToShortDateString();
         }
 
 
--- AcmeApp/Tests/Acme.BizTests/ProductTests.cs
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -24,7 +24,7 @@ namespace Acme.Biz.Tests
             var companyName = currentProduct?.ProductVendor?.CompanyName;
             companyName = "ABC Corp";
 
-            var expected = "Hello Saw (1): 15-inch steel blade hand saw";
+            var expected = "Hello Saw (1): 15-inch steel blade hand saw" + " Available on: ";
 
             //Act
             var actal = currentProduct.SayHello();
@@ -40,7 +40,7 @@ namespace Acme.Biz.Tests
             //Arrange
             var currentProduct = new Product(1,"Saw", "15-inch steel blade hand saw");
             
-            var expected = "Hello Saw (1): 15-inch steel blade hand saw";
+            var expected = "Hello Saw (1): 15-inch steel blade hand saw" + " Available on: ";
 
             //Act
             var actual = currentProduct.SayHello(); 
@@ -61,13 +61,13 @@ namespace Acme.Biz.Tests
                 Description = "15-inch steel blade hand saw"
             };
 
-            var excepted = "Hello Saw (1): 15-inch steel blade hand saw";
-            
+            var expected = "Hello Saw (1): 15-inch steel blade hand saw" + " Available on: ";
+
             //Act
             var actual = currentProduct.SayHello();
 
             //Assert
-            Assert.AreEqual(excepted,actual);
+            Assert.AreEqual(expected,actual);
         }
 
         [TestMethod]
@@ -77,13 +77,201 @@ namespace Acme.Biz.Tests
             Product currentProduct = null;
             var companyName = currentProduct?.ProductVendor?.CompanyName;
 
-            string excepted = null;
+            string expected = null;
 
             //Act
             var actual = companyName;
 
             //Assert
-            Assert.AreEqual(excepted, actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void ConvertMetersToInchesTest()
+        {
+            //Arrange
+            var expected = 78.74;
+
+            //Act
+            var actual = 2 * Product.InchesPerMEter;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MinimumPriceTest_Default()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            var expected = .96m;
+
+            //Act
+            var actual = currentProduct.MinimumPrice;
+            
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MinimumPriceTest_Game()
+        {
+            //Arrange
+            var currentProduct = new Product(1, "GameOfThrone", "cooming soon");
+            var expected = 9.99m;
+
+            //Act
+            var actual = currentProduct.MinimumPrice;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ProductName_Format()
+        {
+            //arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "    Steel Hammer  ";
+            var expected = "Steel Hammer";
+
+            //act
+            var actual = currentProduct.ProductName;
+
+            //assert
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void ProductNameSet_TooShort()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "aw";
+
+            string expected = null;
+            string expectedMessage = "Product Name must be at least 3 characters.";
+
+            //Act
+            var actual = currentProduct.ProductName;
+            var actualMessage = currentProduct.ValidationMassage;
+
+            //Assert
+            Assert.AreEqual(expected,actual);
+            Assert.AreEqual(expectedMessage,actualMessage);
+        }
+
+        [TestMethod]
+        public void ProductNameSet_TooLong()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "abcefghijklmnoprstuwxyz";
+
+            string expected = null;
+            string expectedMessage = "Product Name cannot be more than 20 characters.";
+
+            //Act
+            var actual = currentProduct.ProductName;
+            var actualMessage = currentProduct.ValidationMassage;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [TestMethod]
+        public void ProductNameSet_Good()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "Whisky";
+
+            string expected = "Whisky";
+            string expectedMessage = null;
+
+            //Act
+            var actual = currentProduct.ProductName;
+            var actualMessage = currentProduct.ValidationMassage;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [TestMethod]
+        public void Cetegory_DefaultValue()
+        {
+            //arrange
+            var currentProduct = new Product();
+            var expected = "Tools";
+
+            //act
+            var actual = currentProduct.Category;
+
+            //assert
+            Assert.AreEqual(expected,actual);
+        }
+
+
+        [TestMethod]
+        public void Cetegory_NewValue()
+        {
+            //arrange
+            var currentProduct = new Product();
+            currentProduct.Category = "Garden";
+            var expected = "Garden";
+
+            //act
+            var actual = currentProduct.Category;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Sequence_DefaultValue()
+        {
+            //arrange
+            var currentProduct = new Product();
+            var expected = 1;
+
+            //act
+            var actual = currentProduct.SequenceNumber;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Sequence_NewValue()
+        {
+            //arrange
+            var currentProduct = new Product();
+            currentProduct.SequenceNumber = 2;
+            var expected = 2;
+
+            //act
+            var actual = currentProduct.SequenceNumber;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ProductCode_DefaultValue()
+        {
+            //arrange
+            var currentProduct = new Product();
+            var expected = "Tools-1";
+
+            //act
+            var actual = currentProduct.ProductCode;
+
+            //assert
+            Assert.AreEqual(expected,actual);
+
         }
     }
 }
